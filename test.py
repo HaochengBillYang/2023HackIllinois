9ec6add8c8d623a90952d63332488dcbd7e20c80
bill-the-sci-guy <bill_the_sci_guy@sina.com>
2023-02-25 22:17:35 -0600

    pqr is furious

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from scipy import signal
 from scipy import ndimage
-from PIL import Image
+from PIL import Image, ImageFilter
 import numpy as np
 
 
@@ -12,7 +12,7 @@ def image2arr(path: str):
 
 
 def arr2image(path: str, img_arr: np.array):
-    img_arr = (img_arr).astype(np.uint8)
+    img_arr = img_arr.astype(np.uint8)
     img = Image.fromarray(img_arr)
     img.save(path)
     print('saved to: ' + str(path))
@@ -26,9 +26,6 @@ def d(img_arr: np.array, n: int):
     img_green = img_green[::n, ::n]
     img_blue = img_blue[::n, ::n]
     ans_arr = np.stack((img_red, img_green, img_blue), axis=-1)
-    print(img_red.shape)
-    print(img_arr.shape)
-    print(ans_arr.shape)
     return ans_arr
 
 
@@ -40,93 +37,88 @@ def u(img_arr: np.array, n: int):
     img_green = img_green.repeat(n, axis=0).repeat(n, axis=1)
     img_blue = img_blue.repeat(n, axis=0).repeat(n, axis=1)
     ans_arr = np.stack((img_red, img_green, img_blue), axis=-1)
-    print(img_red.shape)
-    print(img_arr.shape)
-    print(ans_arr.shape)
     return ans_arr
 
 
-def de_noise(img_arr: np.array, n:int):
-
-
-    # # Provided code:
-    # shape = img_arr.shape
-    # n_rows, n_cols = shape[0], shape[1]
-    # # Code for part 4.a:
-    # ba = [0, 0.1, 0.2, 0.5, 0.6, 1]
-    # da = [1, 0, 1]
-    # fir = signal.remez(25, ba, da, fs=2)
-    # w = signal.windows.gaussian(99, 5.0)
+def de_noise_median(img_arr: np.array):
     img_red = img_arr[..., 0]
     img_green = img_arr[..., 1]
     img_blue = img_arr[..., 2]
-    img_red = ndimage.median_filter(img_red,3)
+    img_red = ndimage.median_filter(img_red, 3)
     img_green = ndimage.median_filter(img_green, 3)
     img_blue = ndimage.median_filter(img_blue, 3)
-
-    # img_red = signal.sepfir2d(img_red, w, w)
-    # img_green = signal.sepfir2d(img_green, w, w)
-    # img_blue = signal.sepfir2d(img_blue, w, w)
-
-    # for i in range(0, n_rows):
-    #     img_red[i, :] = signal.convolve(img_red[i, :], fir, 'same')
-    #
-    # for j in range(0, n_cols):
-    #     img_red[:, j] = signal.convolve(img_red[:, j], fir, 'same')
-    #
-    # for i in range(0, n_rows):
-    #     img_green[i, :] = signal.convolve(img_green[i, :], fir, 'same')
-    #
-    # for j in range(0, n_cols):
-    #     img_green[:, j] = signal.convolve(img_green[:, j], fir, 'same')
-    #
-    # for i in range(0, n_rows):
-    #     img_blue[i, :] = signal.convolve(img_blue[i, :], fir, 'same')
-    #
-    # for j in range(0, n_cols):
-    #     img_blue[:, j] = signal.convolve(img_blue[:, j], fir, 'same')
-
-    # img_red_fft = np.fft.rfft2(img_red)
-    # img_green_fft = np.fft.rfft2(img_green)
-    # img_blue_fft = np.fft.rfft2(img_blue)
-    # nr = 0.000001 * abs(np.max(img_red_fft))
-    # ng = 0.000001 * abs(np.max(img_green_fft))
-    # nb = 0.000001 * abs(np.max(img_blue_fft))
-
-    # rows, cols = img_red_fft.shape
-    # for i in range(0, rows):
-    #     for j in range(0, cols):
-    #         if abs(img_red_fft[i,j]) < nr:
-    #             img_red_fft[i,j] = 0
-    # rows, cols = img_green_fft.shape
-    # for i in range(0, rows):
-    #     for j in range(0, cols):
-    #         if abs(img_green_fft[i,j]) < ng:
-    #             img_green_fft[i,j] = 0
-    # rows, cols = img_blue_fft.shape
-    # for i in range(0, rows):
-    #     for j in range(0, cols):
-    #         if abs(img_blue_fft[i,j]) < nb:
-    #             img_blue_fft[i,j] = 0
-    # img_red = np.fft.irfft2(img_red_fft)
-    # img_green = np.fft.irfft2(img_green_fft)
-    # img_blue = np.fft.irfft2(img_blue_fft)
     ans_arr = np.stack((img_red, img_green, img_blue), axis=-1)
+    return ans_arr
+
+
+def de_noise_gaussian(img_arr: np.array, n: int):
+    img_red = img_arr[..., 0]
+    img_green = img_arr[..., 1]
+    img_blue = img_arr[..., 2]
+    img_red = ndimage.gaussian_filter(img_red, n)
+    img_green = ndimage.gaussian_filter(img_green, n)
+    img_blue = ndimage.gaussian_filter(img_blue, n)
+    ans_arr = np.stack((img_red, img_green, img_blue), axis=-1)
+    return ans_arr
+
+
+def enhance(img_arr: np.array):
+    oprand = de_noise_gaussian(d(de_noise_gaussian(u(img_arr, 4), 2), 2), 1)
+    return oprand
+
+
+def sharpen(img_arr: np.array):
+    alpha = 0.01
+    return img_arr + alpha * (img_arr - de_noise_gaussian(img_arr, 1))
+
+
+def edge(img_arr: np.array):
+    img_red = img_arr[..., 0]
+    img_green = img_arr[..., 1]
+    img_blue = img_arr[..., 2]
+
+    img_arr = (img_red + img_blue + img_green) / 3
+
+    h = [1, -1]
+
+    a_arr = np.zeros(img_arr.shape)
+    for n in range(img_arr.shape[0]):
+        a_arr[n, :] = signal.convolve(img_arr[n, :], h, 'same')
+
+    b_arr = np.zeros(img_arr.shape)
+    for n in range(img_arr.shape[1]):
+        b_arr[:, n] = signal.convolve(img_arr[:, n], h, 'same')
+
+    ans_arr = np.zeros(img_arr.shape)
+    for i in range(img_arr.shape[0]):
+        for j in range(img_arr.shape[1]):
+            ans_arr[i, j] = ((a_arr[i, j]) ** 2 + (b_arr[i, j]) ** 2) ** 0.5
 
     return ans_arr
 
 
+def invert(img_arr: np.array):
+    ones = np.ones_like(img_arr) * 235
+    return ones - img_arr
+
+
 if __name__ == "__main__":
-
-    TEST_IMAGE_ADDR = 'noise.jpg'
+    TEST_IMAGE_ADDR = 'pictures/hamster.jpg'
     IMG_ARR = image2arr(TEST_IMAGE_ADDR)
-    arr2image('original.jpg', IMG_ARR)
+    arr2image('pictures/original.jpg', IMG_ARR)
     ANS_ARR = u(IMG_ARR, 3)
-    arr2image('upsample.jpg', ANS_ARR)
+    arr2image('pictures/upsample.jpg', ANS_ARR)
     ANS_ARR = d(IMG_ARR, 3)
-    arr2image('downsample.jpg', ANS_ARR)
-    ANS_ARR = de_noise(IMG_ARR, 10)
-    arr2image('denoised.jpg', ANS_ARR)
-
-
-
+    arr2image('pictures/downsample.jpg', ANS_ARR)
+    ANS_ARR = de_noise_median(IMG_ARR)
+    arr2image('pictures/median.jpg', ANS_ARR)
+    ANS_ARR = de_noise_gaussian(IMG_ARR, 2)
+    arr2image('pictures/gauss.jpg', ANS_ARR)
+    ANS_ARR = enhance(IMG_ARR)
+    arr2image('pictures/enhanced.jpg', ANS_ARR)
+    ANS_ARR = sharpen(IMG_ARR)
+    arr2image('pictures/sharpened.jpg', ANS_ARR)
+    ANS_ARR = edge(IMG_ARR)
+    arr2image('pictures/edge.jpg', ANS_ARR)
+    ANS_ARR = invert(edge(IMG_ARR))
+    arr2image('pictures/sketch.jpg', ANS_ARR)
